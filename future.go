39d4a07e864ad926826c6b27a1b72aecc78ce89d
christianfoleide <christian.foleide@gmail.com>
2021-09-14 18:47:48 +0200

    add IsDone() and IsCancelled()

--- future.go
+++ future.go
@@ -3,6 +3,7 @@ package future
 import (
 	"context"
 	"errors"
+	"sync"
 )
 
 // FutureFunc is the function passed to the future.InvokeAsync method.
@@ -19,6 +20,10 @@ type futureHandle struct {
 	resChan    chan interface{}
 	errChan    chan error
 	cancelChan chan struct{}
+
+	mu          sync.RWMutex
+	isCancelled bool
+	isDone      bool
 }
 
 // InvokeAsync invokes the given FutureFunc on a new goroutine, and immediately returns
@@ -29,6 +34,7 @@ func InvokeAsync(fn FutureFunc) *futureHandle {
 		resChan:    make(chan interface{}, 1),
 		errChan:    make(chan error, 1),
 		cancelChan: make(chan struct{}, 1),
+		mu:         sync.RWMutex{},
 	}
 
 	go func(h *futureHandle) {
@@ -57,10 +63,13 @@ func InvokeAsync(fn FutureFunc) *futureHandle {
 
 		select {
 		case <-h.cancelChan:
+			h.setCancelled()
 			h.errChan <- ErrFutureCancelled
 		case err := <-errPipe:
+			h.setDone()
 			h.errChan <- err
 		case res := <-resPipe:
+			h.setDone()
 			h.resChan <- res
 		}
 
@@ -71,6 +80,7 @@ func InvokeAsync(fn FutureFunc) *futureHandle {
 // Cancel cancels the receiving future, causing future.Block to return an error and also
 // release resources.
 func (h *futureHandle) Cancel() {
+	h.setCancelled()
 	h.cancelChan <- struct{}{}
 }
 
@@ -80,6 +90,7 @@ func (h *futureHandle) Block(ctx context.Context) (interface{}, error) {
 	select {
 	case <-ctx.Done():
 		if ctx.Err() == context.Canceled {
+			h.setCancelled()
 			return nil, ErrFutureCancelled
 		}
 		return nil, ctx.Err() // context.DeadlineExceeded
@@ -88,4 +99,29 @@ func (h *futureHandle) Block(ctx context.Context) (interface{}, error) {
 	case res := <-h.resChan:
 		return res, nil
 	}
+}
+
+func (h *futureHandle) IsCancelled() bool {
+	h.mu.RLocker().Lock()
+	defer h.mu.RLocker().Unlock()
+	return h.isCancelled
+}
+
+func (h *futureHandle) IsDone() bool {
+	h.mu.RLocker().Lock()
+	defer h.mu.RLocker().Unlock()
+	return h.isDone
+}
+
+func (h *futureHandle) setCancelled() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.isCancelled = true
+	h.isDone = true
+}
+
+func (h *futureHandle) setDone() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.isDone = true
 }
--- future_test.go
+++ future_test.go
@@ -66,9 +66,17 @@ func TestCancelFuture(t *testing.T) {
 	if err != ErrFutureCancelled {
 		t.Errorf("expected Block to return ErrFutureCanceled after cancellation but didn't")
 	}
+
+	if !fut.IsCancelled() {
+		t.Errorf("expected fut.IsCancelled() to be true but wasn't")
+	}
+
+	if !fut.IsDone() {
+		t.Errorf("expected a cancelled future to also be done but wasn't")
+	}
 }
 
-func TestTimeoutFuture(t *testing.T) {
+func TestTimeoutRetryFuture(t *testing.T) {
 	defer func() {
 		time.Sleep(sleepDur)
 		goleak.VerifyNone(t)
@@ -85,6 +93,15 @@ func TestTimeoutFuture(t *testing.T) {
 	_, err := fut.Block(ctx)
 	if err != context.DeadlineExceeded {
 		t.Errorf("expected err to be 'context deadline exceeded' but was %+v", err)
+	}
+
+	time.Sleep(sleepDur)
+	res, err := fut.Block(context.Background())
+	if err != nil {
+		t.Errorf("attempted second try failed with err: %+v", err)
+	}
+	if res != "Hello, world!" {
+		t.Errorf("expected result to be 'Hello, world!' but wasn't")
 	}
 }
 
@@ -103,5 +120,9 @@ func TestCancelWithContext(t *testing.T) {
 	_, err := fut.Block(ctx)
 	if err != ErrFutureCancelled {
 		t.Errorf("expected context cancellation to cause ErrFutureCancelled but was: %+v", err)
+	}
+
+	if !fut.IsCancelled() && !fut.IsDone() {
+		t.Errorf("expected future to be both cancelled and done but wasn't")
 	}
 }
